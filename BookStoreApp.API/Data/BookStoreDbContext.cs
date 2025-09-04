8c3fc1f63edd006c643ddc91c3d00151d8da5eb0
sdusama <sdusama@gmail.com>
2025-09-04 17:22:00 +0100

    Updated Authors
    Add author management features and database integration
    
    - Added package references for AutoMapper and Entity Framework Core.#
    - Implemented `MapperConfig` for DTO mappings.
    - Enhanced `AuthorsController` with CRUD operations and logging.
    - Created data model classes: `Author`, `Book`, and `BookStoreDbContext`.
    - Defined DTOs for author data transfer.
    - Updated `Program.cs` for service configuration.
    - Developed Razor views for author management.
    - Included client-side validation scripts.
    - Configured database connection in `appsettings.json`.

---
+++ BookStoreApp.API/Data/BookStoreDbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApp.API.Data;
+
+public partial class BookStoreDbContext : DbContext
+{
+    public BookStoreDbContext()
+    {
+    }
+
+    public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost\\sqlexpress;Database=BookStoreDb;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Authors__3214EC07A77C7CE1");
+
+            entity.Property(e => e.Bio).HasMaxLength(50);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.Property(e => e.Isbn).HasMaxLength(20);
+            entity.Property(e => e.Summary).HasMaxLength(60);
+            entity.Property(e => e.Title).HasMaxLength(30);
+
+            entity.HasOne(d => d.Author).WithMany(p => p.Books).HasForeignKey(d => d.AuthorId);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
